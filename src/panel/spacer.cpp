d1405077035eca0e01173b2077174278811983f2
Simon Long <simon@raspberrypi.com>
2026-03-02 13:36:54 +0000

    Hide separators at edges

--- src/panel/spacer.cpp
+++ src/panel/spacer.cpp
@@ -23,6 +23,16 @@ void WayfireSpacing::init(Gtk::HBox *container)
 
 bool WayfireSpacing::draw(const Cairo::RefPtr<Cairo::Context>& cr)
 {
+    Gtk::Allocation palloc, alloc = box.get_allocation ();
+    Gtk::Widget *w = dynamic_cast<Gtk::Widget*> (&box);
+    while (w)
+    {
+        palloc = w->get_allocation ();
+        w = w->get_parent ();
+    }
+
+    if (alloc.get_x () == 0 || alloc.get_x () + 1 == palloc.get_width ()) return true;
+
     Glib::RefPtr <Gtk::StyleContext> sc = da.get_style_context ();
     Gdk::RGBA fg = sc->get_color ();
     int height = da.get_allocated_height ();
