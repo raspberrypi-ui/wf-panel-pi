f0b6bb7191b77d3b847abd3656c65f9e2a2e8a79
Simon Long <simon@raspberrypi.com>
2026-03-11 11:06:39 +0000

    Don't offset notifications with the dock at the top

--- src/panel/notification.c
+++ src/panel/notification.c
@@ -132,7 +132,7 @@ static void show_message (NotifyWindow *nw, char *str)
 
     // calculate vertical offset for new window - if critical, at top, else immediately below any criticals
     if (gtk_layer_get_exclusive_zone (GTK_WINDOW (wpanel))) offset = 0;
-    else if (panel_at_bottom (wpanel)) offset = get_icon_size (wdock);
+    else if (panel_at_bottom (wpanel)) offset = 0; //!!!!! maybe get_icon_size (wdock);
     else offset = get_icon_size (wpanel);
 
     offset += SPACING;
