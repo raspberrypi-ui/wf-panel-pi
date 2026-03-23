649824fcb81952b246c8ebfdbd829f5c03dfdba8
Simon Long <simon@raspberrypi.com>
2026-03-23 11:02:11 +0000

    Fix warnings from icon size function

--- src/panel/lxutils.c
+++ src/panel/lxutils.c
@@ -107,7 +107,7 @@ gboolean panel_at_bottom (GtkWidget *btn)
 int get_icon_size (GtkWidget *widget)
 {
     GtkWindow *panel = find_panel (widget);
-    if (!g_strcmp0 (gtk_widget_get_name (GTK_WIDGET (panel)), "DockToplevel")) return d_icon_size;
+    if (panel && GTK_IS_WIDGET (panel) && !g_strcmp0 (gtk_widget_get_name (GTK_WIDGET (panel)), "DockToplevel")) return d_icon_size;
     else return p_icon_size;
 }
 
