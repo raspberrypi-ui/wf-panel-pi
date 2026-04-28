b7100b5cdae5691c87be002ddda090c4e13db026
Simon Long <simon@raspberrypi.com>
2026-04-28 08:57:27 +0100

    Whoops

--- src/panel/lxutils.c
+++ src/panel/lxutils.c
@@ -599,7 +599,7 @@ void popup_window_at_button (GtkWidget *window, GtkWidget *button)
     bottom = gtk_layer_get_anchor (panel, GTK_LAYER_SHELL_EDGE_BOTTOM);
     gtk_widget_get_allocation (GTK_WIDGET (panel), &rect);
     px = rect.width;
-    if (bottom) py = gtk_layer_get_margin (panel, bottom ? GTK_LAYER_SHELL_EDGE_BOTTOM : GTK_LAYER_SHELL_EDGE_TOP);
+    py = gtk_layer_get_margin (panel, bottom ? GTK_LAYER_SHELL_EDGE_BOTTOM : GTK_LAYER_SHELL_EDGE_TOP);
     if (gtk_layer_get_exclusive_zone (panel) <= 0) py += rect.height;
     panw = px;
 
