3d8d52857b97d05b2a3430c67297d32992d0c3cb
Simon Long <simon@raspberrypi.com>
2026-06-15 10:57:36 +0100

    Grrr - exclusive zones...

--- src/panel/notification.c
+++ src/panel/notification.c
@@ -508,7 +508,8 @@ static void show_message (NotifyWindow *nw, char *str)
     }
 
     // calculate vertical offset for new window - if critical, at top, else immediately below any criticals
-    if (gtk_layer_get_exclusive_zone (panel)) offset = 0;
+    if (gtk_layer_get_exclusive_zone (panel) && gtk_layer_get_anchor (panel, GTK_LAYER_SHELL_EDGE_LEFT)
+        && gtk_layer_get_anchor (panel, GTK_LAYER_SHELL_EDGE_RIGHT)) offset = 0;
     else if (panel_at_bottom (GTK_WIDGET (panel))) offset = 0;
     else if (!gtk_widget_get_visible (GTK_WIDGET (panel))) offset = 0;
     else offset = get_icon_size (GTK_WIDGET (panel));
