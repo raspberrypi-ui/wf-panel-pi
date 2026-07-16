20f9f0d7f2804c432f17763b2337cd39be2ba6a0
Simon Long <simon@raspberrypi.com>
2026-07-16 11:25:24 +0100

    Remove redundant array

--- src/wf-panel-pi/panel-app.hpp
+++ src/wf-panel-pi/panel-app.hpp
@@ -55,8 +55,6 @@ class PanelApp
     std::unique_ptr <Panel> panel;
     std::unique_ptr <Panel> dock;
 
-    std::vector <Glib::RefPtr <Gdk::Monitor>> monitors;
-
     sigc::connection hotplug_timer;
 
     Glib::RefPtr <Gio::DBus::NodeInfo> introspection_data;
